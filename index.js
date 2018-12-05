1f5a13dea940461ab4e8f4424629401394ab9a07
fadirs <45557717+fadirs@users.noreply.github.com>
2018-12-05 06:32:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const bodyParser = require("body-parser");
 const restService = express();
 
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'us-cdbr-iron-east-01.cleardb.net',
-  user     : 'b0cb1ef1838d5e',
-  password : 'a26fe726',
-  database : 'heroku_9e5a751b12d72df'
-});
+
 
 
 
